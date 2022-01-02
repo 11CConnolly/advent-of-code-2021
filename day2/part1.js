06ebd3bed4a85822086d13fc63d7ce57e9f9fdc5
11cconnolly <callumc11@gmail.com>
2022-01-02 12:27:10 +0000

    Day 2 Part 1

---
+++ day2/part1.js
@@ -0,0 +1,26 @@
+const {input} = require("./input.js");
+
+const instructions = input.map(x => x.split(/[ ,]+/));
+
+let horizDiff = 0
+let vertDiff = 0
+
+for (let i = 0; i < instructions.length; i++) {
+
+  const instruction = instructions[i][0];
+  const distance = Number(instructions[i][1]);
+  
+  switch (instruction) {
+    case "forward": 
+      horizDiff += distance;
+      break;
+    case "down": 
+      vertDiff += distance;
+      break;
+    case "up": 
+      vertDiff -= distance;
+      break;
+  }
+}
+
+console.log(`${horizDiff} and ${vertDiff} for final answer ${horizDiff * vertDiff}`);
