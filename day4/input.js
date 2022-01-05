e3031d2c3ffc576fe478ae6a0a9a8ff5be934cac
11cconnolly <callumc11@gmail.com>
2022-01-05 20:14:44 +0000

    Boards input for Day 4

--- day4/input.js
+++ day4/input.js
@@ -1,9 +1,31 @@
 const fs = require("fs");
 
-const input = fs
+const lines = fs
         .readFileSync("input.txt", "utf8")
         .toString()
         .split(/\r?\n/);
+
+// Get array of numbers
+const pickNumbers = lines[0];
+
+// Get array of boards
+const boardLines = lines.slice(2).map(x => x.trim().split(/\s+/));
+let boards = [];
+let currentBoard = 0;
+
+for (let i = 0; i <= boardLines.length; i+= 6) {
+  let newBoardsLine = [];
+  for (let j = 0; j < 5; j++) {
+    newBoardsLine.push(boardLines[i + j].slice());
+  }
+  boards[currentBoard] = newBoardsLine;
+  currentBoard++;
+}
+
+const input = {
+  pickNumbers,
+  boards
+}
 
 module.exports = {
   input,
--- day4/part1.js
+++ day4/part1.js
@@ -1,5 +1,6 @@
 const { input } = require('./input');
 
+const { pickNumbers, boards } = input;
 
-
-console.log(input);
+console.log(pickNumbers);
+console.log(boards);
