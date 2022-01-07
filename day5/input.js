2b5e5a8bca111deed564933de4ed0453e53b552b
11cconnolly <callumc11@gmail.com>
2022-01-07 22:18:15 +0000

    Input for day 5

--- day5/input.js
+++ day5/input.js
@@ -1,10 +1,19 @@
 const fs = require("fs");
 
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
 const input = fs
         .readFileSync("input.txt", "utf8")
         .toString()
-        .split(/\r?\n/);
+        .split(/\r?\n/)
+        .map(x => x.split("->").map(s => s.trim()))
 
 module.exports = {
   input,
+  Point
 }
--- day5/part1.js
+++ day5/part1.js
@@ -1,5 +1,13 @@
-const { input } = require('./input');
+const { input, Point } = require('./input');
+
+//class Point {
+//  constructor(x, y) {
+//    this.x = x;
+//    this.y = y;
+//  }
+//}
 
 
 
 console.log(input);
+console.log(Point);
