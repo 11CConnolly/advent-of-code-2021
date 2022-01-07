db1b85dea9ecfdfd96cf25e8f08f04f26151e1f7
11cconnolly <callumc11@gmail.com>
2022-01-07 14:09:16 +0000

    Clean up

--- day4/part1.js
+++ day4/part1.js
@@ -3,48 +3,36 @@ const { input } = require('./input');
 const { pickNumbers, boards } = input;
 
 const checkRowOrColumnIsComplete = (schlength) => {
-  let complete = true;
-  schlength.forEach(x => {
-    if (x !== "-1")
-      complete = false;
-  });
-  if (complete)
-  return complete;
+  return schlength.filter(x => x === "-1").length === 5;
 }
 
 const boardIsComplete = (board) => {
-  board.forEach(row => {
-    if (checkRowOrColumnIsComplete(row)) {
-      return true;
-    }
-  })
-  
-  for (let i = 0; i < board.length; i++) {
+  for (let i = 0; i < 5; i++) {
+    let rowLine = [];
     let columnLine = [];
-    for (let j = 0; j < board[0].length; j++) {
-      columnLine.push(board[i][j]);
+    for (let j = 0; j < 5; j++) {
+      rowLine.push(board[i][j]);
+      columnLine.push(board[j][i]);
     }
-    if (checkRowOrColumnIsComplete(columnLine)) {
+    
+    if (checkRowOrColumnIsComplete(rowLine) || checkRowOrColumnIsComplete(columnLine))
       return true;
-    }
   }
   
   return false;
 }
 
 const markBoard = (board, pick) => {
-  let marked = false;
-  
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[0].length; j++) {
       if (Number(board[i][j]) === pick) {
         board[i][j] = "-1";
-        marked = true;
+        return true;
       }
     }
   }
   
-  return marked;
+  return false;
 }
 
 const findFirstWinningBoard = (numbers, boards) => {
@@ -65,12 +53,11 @@ const findFirstWinningBoard = (numbers, boards) => {
 
 const { firstBoard, firstPick } = findFirstWinningBoard(pickNumbers.split(',').map(Number), boards);
 
+// Calculate Score
 let total = 0;
-
 firstBoard.forEach(line => {
   line.forEach(number => 
     number !== "-1" && (total += Number(number))
   )
 })
-
 console.log(total * firstPick);
--- day4/part2.js
+++ day4/part2.js
@@ -1,5 +1,63 @@
-let { input } = require('./input');
+const { input } = require('./input');
 
+const { pickNumbers, boards } = input;
 
+const checkRowOrColumnIsComplete = (schlength) => {
+  return schlength.filter(x => x === "-1").length === 5;
+}
 
-console.log(input);
+const boardIsComplete = (board) => {
+  for (let i = 0; i < 5; i++) {
+    let rowLine = [];
+    let columnLine = [];
+    for (let j = 0; j < 5; j++) {
+      rowLine.push(board[i][j]);
+      columnLine.push(board[j][i]);
+    }
+    
+    if (checkRowOrColumnIsComplete(rowLine) || checkRowOrColumnIsComplete(columnLine))
+      return true;
+  }
+  
+  return false;
+}
+
+const markBoard = (board, pick) => {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (Number(board[i][j]) === pick) {
+        board[i][j] = "-1";
+        return true;
+      }
+    }
+  }
+  
+  return false;
+}
+
+const findFirstWinningBoard = (numbers, boards) => {
+  let firstBoard = [];
+  let firstPick = 0;
+  numbers.every(pick => {
+    firstBoard = boards.find(board => markBoard(board, pick) && boardIsComplete(board));
+    if (firstBoard !== undefined) {
+      firstPick = pick;
+      return false;
+    }
+    
+    return true;
+  })
+  
+  return { firstBoard, firstPick }
+}
+
+const { firstBoard, firstPick } = findFirstWinningBoard(pickNumbers.split(',').map(Number), boards);
+
+// Calculate Score
+let total = 0;
+firstBoard.forEach(line => {
+  line.forEach(number => 
+    number !== "-1" && (total += Number(number))
+  )
+})
+console.log(total * firstPick);
