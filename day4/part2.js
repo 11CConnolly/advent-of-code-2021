e27c2890c9f05e405099f5650fdf9a316cdbc610
11cconnolly <callumc11@gmail.com>
2022-01-07 16:51:50 +0000

    Day 4 part 2 Complete

--- day4/part2.js
+++ day4/part2.js
@@ -13,8 +13,7 @@ const boardIsComplete = (board) => {
       rowLine.push(board[i][j]);
     }
     
-    if (checkRowOrColumnIsComplete(rowLine))
-      return true;
+    if (checkRowOrColumnIsComplete(rowLine)) return true;
   }
   
   for (let i = 0; i < 5; i++) {
@@ -23,8 +22,7 @@ const boardIsComplete = (board) => {
       columnLine.push(board[j][i]);
     }
     
-    if (checkRowOrColumnIsComplete(columnLine))
-      return true;
+    if (checkRowOrColumnIsComplete(columnLine)) return true;
   }
   
   return false;
@@ -43,47 +41,31 @@ const markBoard = (board, pick) => {
   return false;
 }
 
-const findLastWinningBoard = (numbers, boards) => {
-  let winningBoard = [];
-  let calledPick = 0;
-  numbers.every(pick => {
-    winningBoard = boards.find(board => markBoard(board, pick) && boardIsComplete(board));
-    console.log(winningBoard);
-    if (winningBoard !== undefined) {
-      boards.splice(boards.indexOf(winningBoard), 1);
-      if (boards.length === 0)
-        return false;
-    }
-    
-    return true;
-  })
-  
-  return { winningBoard, calledPick }
-}
+const winningOrder = [];
+let finalPickedNumber = 0;
 
 const findLastWinningBoard = (numbers, boards) => {
-  let winningBoard = []
-  let calledNumber = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    winningBoard = boards.find(board => markBoard(board, numbers[i]) && boardIsComplete(board));
-    if (winningBoard !== undefined) {
-      calledNumber = numbers[i]
-      boards.splice(boards.indexOf(winningBoard), 1);
-      
-      if (boards.length === 0) return true;
+  for (let pick of numbers) {
+    for (let i = 0; i < boards.length; i++) {
+      if (winningOrder.indexOf(i) !== -1) {
+        continue;
+      }
+      if (markBoard(boards[i], pick) && boardIsComplete(boards[i])) {
+          finalPickedNumber = pick;
+          winningOrder.push(i);
+      }
     }
   }
-  
-  return { winningBoard, calledNumber }
 }
 
-const { winningBoard, calledPick } = findLastWinningBoard(pickNumbers.split(',').map(Number), boards);
+findLastWinningBoard(pickNumbers.split(',').map(Number), boards);
 
 // Calculate Score
 let total = 0;
-winningBoard.forEach(line => {
+boards[winningOrder[winningOrder.length - 1]].forEach(line => {
   line.forEach(number => 
     number !== "-1" && (total += Number(number))
   )
 })
-console.log(total * calledPick);
+
+console.log(total * finalPickedNumber);
