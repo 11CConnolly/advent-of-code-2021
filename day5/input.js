78ffd376f6283083639678e9027c7cd5edf2a779
11cconnolly <callumc11@gmail.com>
2022-01-10 21:00:39 +0000

    Progress with Day 5 part 1

--- day5/input.js
+++ day5/input.js
@@ -17,13 +17,46 @@ class Pair {
     return (this.first.x === this.second.x || 
              this.first.y === this.second.y)
   }
+  
+  calculateLine() {
+    if (!this.isStraightLine)
+      return undefined;
+    
+    let line = [];
+    line.push(this.first);
+    
+    let smaller = 0;
+    let x_difference = Math.abs(this.first.x - this.second.x);
+    let y_difference = Math.abs(this.first.y - this.second.y);
+    
+    // Still Ugly
+    if (x_difference > 1) {
+      this.first.x < this.second.x 
+        ? smaller = this.first.x 
+        : smaller = this.second.x
+      for (let i = 1; i < x_difference; i++) {
+        line.push(new Point(smaller + i, this.first.y))
+      }
+    } else if (y_difference > 1) {
+      this.first.y < this.second.y 
+        ? smaller = this.first.y 
+        : smaller = this.second.y
+      for (let i = 1; i < y_difference; i++) {
+        line.push(new Point(this.first.x, smaller + i))
+      }
+    }
+    
+    line.push(this.second);
+    
+    return line;
+  }
 }
 
 let makeNewPair = (coords) => {
   const p1 = coords[0].split(",");
   const p2 = coords[1].split(",");
-  const pointOne = new Point(p1[0], p1[1]);
-  const pointTwo = new Point(p2[0], p2[1]);
+  const pointOne = new Point(+p1[0], +p1[1]);
+  const pointTwo = new Point(+p2[0], +p2[1]);
   
   return new Pair(pointOne, pointTwo);
 }
--- day5/part1.js
+++ day5/part1.js
@@ -1,29 +1,32 @@
 const { input, Point, Pair } = require('./input');
 
-console.log(input);
-console.log(Point);
+// Find max size of board X and Y
+// There's a better way to do this, probably
+let maxX = 0;
+let maxY = 0;
 
-
-
-
-
-
-
-//class Point {
-//  constructor(x, y) {
-//    this.x = x;
-//    this.y = y;
-//  }
-//}
-
-//class Pair {
-//  constructor(first, second) {
-//    this.first = first;
-//    this.second = second;
-//  }
+input.map(pair => {
+  const x_ = pair.first.x;
+  const y_ = pair.first.y;
+  const _x = pair.second.x;
+  const _y = pair.second.y;
   
-//  get isStraightLine() {
-//    return (this.first.x === this.second.x || 
-//             this.first.y === this.second.y)
-//  }
-//}
+  x_ > maxX && (maxX = x_)
+  y_ > maxY && (maxY = y_)
+  _x > maxX && (maxX = _x)
+  _y > maxY && (maxY = _y)
+})
+
+const board = new Array(maxX).fill(new Array(maxY).fill("."));
+
+input.map(x => {
+  const line = x.calculateLine();
+  line && line.map(p => {
+      const currentPosition = board[p.x][p.y];
+      board[p.x][p.y] = currentPosition === '.' 
+        ? 1
+        : board[p.x][p.y]++;
+    })
+});
+
+console.log(board);
