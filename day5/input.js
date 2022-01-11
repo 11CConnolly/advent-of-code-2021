4b8912bc0ede042e0bbac9ba4e13c4a4a1dce64f
11cconnolly <callumc11@gmail.com>
2022-01-11 20:08:38 +0000

    Day 5 part 2 progress

--- day5/input.js
+++ day5/input.js
@@ -20,31 +20,61 @@ class Pair {
   
   calculateLine() {
     if (!this.isStraightLine)
-      return undefined;
+      return calculateDiagLine();
     
     let line = [];
     line.push(this.first);
     
     let smaller = 0;
-    let x_difference = Math.abs(this.first.x - this.second.x);
-    let y_difference = Math.abs(this.first.y - this.second.y);
+    let x_diff = Math.abs(this.first.x - this.second.x);
+    let y_diff = Math.abs(this.first.y - this.second.y);
     
     // Still Ugly
-    if (x_difference > 1) {
+    if (x_diff > 1) {
       this.first.x < this.second.x 
         ? smaller = this.first.x 
         : smaller = this.second.x
-      for (let i = 1; i < x_difference; i++) {
+      for (let i = 1; i < x_diff; i++) {
         line.push(new Point(smaller + i, this.first.y))
       }
-    } else if (y_difference > 1) {
+    } else if (y_diff > 1) {
       this.first.y < this.second.y 
         ? smaller = this.first.y 
         : smaller = this.second.y
-      for (let i = 1; i < y_difference; i++) {
+      for (let i = 1; i < y_diff; i++) {
         line.push(new Point(this.first.x, smaller + i))
       }
     }
+    
+    line.push(this.second);
+    
+    return line;
+  }
+  
+  calculateDiagLine() {
+    let line = [];
+    line.push(this.first);
+    
+    let smaller = 0;
+    let x_diff = Math.abs(this.first.x - this.second.x);
+    let y_diff = Math.abs(this.first.y - this.second.y);
+    
+    if (this.first.x === this.first.y) {
+      this.first.x < this.second.x 
+        ? smaller = this.first.x;
+        : smaller = this.first.y;
+      for (let i = 1; i < x_diff; i++) {
+        line.push(new Point(smaller + i, smaller + i))
+      }
+    } else if (this.first.x < this.second.x) {
+      // Do something else
+    } else if (this.first.x > this.second.x) {
+      // Do something else
+    }
+    
+    let smaller = 0;
+    
+    
     
     line.push(this.second);
     
