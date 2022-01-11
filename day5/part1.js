8aaa5f18e19a108cde81250677d9afedce197074
11cconnolly <callumc11@gmail.com>
2022-01-11 19:38:50 +0000

    Progress with Day5 part1

--- day5/part1.js
+++ day5/part1.js
@@ -17,16 +17,38 @@ input.map(pair => {
   _y > maxY && (maxY = _y)
 })
 
-const board = new Array(maxX).fill(new Array(maxY).fill("."));
+let board = new Array(maxX + 1);
+for (let i = 0; i < maxX + 1; i++) {
+  board[i] = new Array(maxY + 1).fill(".");
+}
+
+console.log(board);
 
 input.map(x => {
   const line = x.calculateLine();
-  line && line.map(p => {
-      const currentPosition = board[p.x][p.y];
-      board[p.x][p.y] = currentPosition === '.' 
-        ? 1
-        : board[p.x][p.y]++;
-    })
+  if (line !== undefined) {
+    for (p of line) {
+      console.log(typeof(p));
+      console.log(p);
+      console.log("===");
+      if (board[p.x][p.y] === ".") {
+        board[p.x][p.y] = 1;
+      } else {
+        board[p.x][p.y]++;
+      }
+    }
+  }
 });
 
-console.log(board);
+let crossovers = 0;
+board.map(e => e.map(x => {x > 1 && (crossovers++)}));
+
+console.log(crossovers);
+
+//Lessons learned
+//- Array.fill() will fill to a static value, even if this value is an object declared as new.
+//It will feed in that same value
+//- When dealing with JS classes, need to use 'this' keyword to refer to properties
+//- Class syntax, use get XYZ for properties, and straight function syntax for methods
+//- Classes need a constructor to initalise values
+//- Be extremely careful in plain JS without TypeScript dealing with datatypes. Downside of weakly typed languages
