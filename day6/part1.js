b405a72884c3ba3228041a49031b0861956c6701
11cconnolly <callumc11@gmail.com>
2022-01-17 18:29:32 +0000

    Day 6 part 1 complete

--- day6/part1.js
+++ day6/part1.js
@@ -1,5 +1,20 @@
 const { input } = require('./input');
 
-console.log(typeof(input));
+// [3, 4, 3, 1, 2]
 
-console.log(input);
+let count = 0;
+
+while (count < 80) {
+  // Maybe can refactor this to filter out all the 0s then each time add an 8? 
+  for (let i=0;i<input.length;i++) {
+    if (input[i] === 0) {
+      input[i] = 7;
+      input.push(9);
+    }
+    input[i]--;
+  }
+  
+  count++;
+}
+ 
+console.log(input.length);
